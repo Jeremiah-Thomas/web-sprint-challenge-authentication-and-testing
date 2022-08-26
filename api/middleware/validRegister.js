def8031dfe7ad54db4555ea3c5c3844afb043d1e
Jeremiah Thomas <jeremiah.b.thomas12@gmail.com>
2022-08-26 16:57:34 -0400

    registration endpoint works

--- api/middleware/validRegister.js
+++ api/middleware/validRegister.js
@@ -2,7 +2,7 @@ const db = require("../../data/dbConfig");
 
 module.exports = async (req, res, next) => {
   const matches = await db("users").where("username", req.body.username);
-  if (req.body.username == null || req.body.password == null) {
+  if (req.body.username == "" || req.body.password == "") {
     res.status(400).json({ message: "username and password required" });
   } else if (matches.length > 0) {
     res.status(400).json({ message: "username taken" });
