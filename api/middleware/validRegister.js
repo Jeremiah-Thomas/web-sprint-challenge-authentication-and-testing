a913287cc4357fb7ea81885df11b04bd7fd75b0f
Jeremiah Thomas <jeremiah.b.thomas12@gmail.com>
2022-08-28 12:28:24 -0400

    registration endpoint works

--- api/middleware/validRegister.js
+++ api/middleware/validRegister.js
@@ -1,12 +1,14 @@
 const db = require("../../data/dbConfig");
 
 module.exports = async (req, res, next) => {
-  const matches = await db("users").where("username", req.body.username);
   if (req.body.username == null || req.body.password == null) {
     res.status(400).json({ message: "username and password required" });
-  } else if (matches.length > 0) {
-    res.status(400).json({ message: "username taken" });
   } else {
-    next();
+    const matches = await db("users").where("username", req.body.username);
+    if (matches.length > 0) {
+      res.status(400).json({ message: "username taken" });
+    } else {
+      next();
+    }
   }
 };
--- api/middleware/validateLogin.js
+++ api/middleware/validateLogin.js
@@ -1,9 +1,8 @@
-const db = require('../../data/dbConfig')
+const db = require("../../data/dbConfig");
 
 module.exports = (req, res, next) => {
-    const [matches]
-    if(req.body.username == null || req.body.password == null ){
-        res.status(400).json({ message: "username and password required" });
-    }
-
-}
+  const matches = db("users").where("usename", req.body.username);
+  if (req.body.username == null || req.body.password == null) {
+    res.status(400).json({ message: "username and password required" });
+  }
+};
